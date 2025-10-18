83fbb39a7f757ee84f1224f2cfecfd7f9798960f
akylbek azhibek <as_azhibek21@mail.ru>
2025-10-18 15:31:41 +0500

    удалил регистрацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {Routes, Route, Navigate} from "react-router-dom";
-import QadamMath from "./pages/QadamMath";
+
 import RegistrationsPage from "./pages/Registrations";
 
 
@@ -8,7 +8,6 @@ import RegistrationsPage from "./pages/Registrations";
 export default function App() {
     return (
         <Routes>
-            <Route path="/" element={<QadamMath/>}/>
 
             <Route path="/qadam-math-2025" element={<Navigate to="/" replace/>}/>
 
